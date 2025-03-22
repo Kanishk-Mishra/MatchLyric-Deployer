b8d6e09b0664f3758728e9018a2c567b21e4aad0
Kanishk Mishra <144137799+Kanishk-Mishra@users.noreply.github.com>
2025-03-22 17:12:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def index():
     # return send_from_directory('../frontend/public', 'index.html')
     return render_template('index.html')
 
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('.', filename)
+
 # @app.route('/src/<path:filename>')
 # def serve_js(filename):
 #     return send_from_directory('../frontend/src', filename)
